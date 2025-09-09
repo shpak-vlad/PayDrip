028aa915b858769c69cd3ae67692eb9b978f66f5
shpak-vlad <shpakvladislavv@outlook.com>
2025-09-09 14:47:52 +0000

    feat: implement stream cloning

---
+++ contracts/src/StreamCloner.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+contract StreamCloner {
+    function cloneStream(uint256 streamId) external pure returns (uint256) {
+        return streamId;
+    }
+}
