5fce356bd595e762a5a795db3ec36eaf7511a311
shpak-vlad <shpakvladislavv@outlook.com>
2025-12-08 14:31:10 +0000

    fix: update ReentrancyGuardUpgradeable import path for OZ v4.9.6
    In OpenZeppelin v4.9.6, ReentrancyGuardUpgradeable is located in the
    security/ directory, not utils/. This change corrects the import paths
    in all contracts using this guard.
    
    Changes:
    - PayDrip.sol: security/ReentrancyGuardUpgradeable
    - BasePayDrip.sol: security/ReentrancyGuardUpgradeable
    - PaymentLinkFactory.sol: security/ReentrancyGuardUpgradeable
    
    This resolves compilation errors in CI where contracts couldn't locate
    the ReentrancyGuardUpgradeable file at the old utils/ path.

--- contracts/src/PayDrip.sol
+++ contracts/src/PayDrip.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.23;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract PayDrip is
--- contracts/src/integrations/PaymentLinkFactory.sol
+++ contracts/src/integrations/PaymentLinkFactory.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.23;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IPayDrip.sol";
 import "./BasePayDrip.sol";
